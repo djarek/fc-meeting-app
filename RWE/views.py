1880e8fdd4bc5ada31ea37cbbd5745871bb70699
Adam <adam@ubuntu.ubuntu-domain>
2016-11-24 21:34:12 +0100

    For VM

--- RWE/views.py
+++ RWE/views.py
@@ -1,7 +1,84 @@
-from django.views.generic import TemplateView
+# -*- encoding: utf-8 -*-
+import ldap
+
+from django.views.generic import TemplateView, View
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group, Permission
 
 from RWE.mixins import LoginRequiredMixin
+from apps.council.models import Person
 
 
 class HomeTemplateView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        email = request.POST.get('email', None)
+        username = email.split('@')[0]
+        password = request.POST.get('password', None)
+
+        try:
+            person = Person.objects.get(email=email)
+        except Person.DoesNotExist:
+            messages.error(request, u'Logowanie zakończyło się niepowodzeniem.')
+            return redirect('login')
+
+        if not person.is_active:
+            messages.error(request, u'Logowanie zakończyło się niepowodzeniem.')
+            return redirect('login')
+
+        if request.POST.get('ldap_login', None):
+            ldap.set_option(ldap.OPT_REFERRALS, 0)
+            ldap.protocol_version = 3
+            conn = ldap.initialize('ldap://z-student.pwr.wroc.pl/')
+            if 'student' in email:
+                q = "uid={}, ou=People, o=student.pwr.wroc.pl, o=pracownicy". \
+                    format(username)
+            else:
+                q = "uid={}, ou=People, o=pwr.wroc.pl, o=pracownicy". \
+                    format(username)
+
+            try:
+                res = conn.simple_bind_s(q, password)
+            except ldap.INVALID_CREDENTIALS:
+                messages.error(request,
+                               u'Logowanie zakończyło się niepowodzeniem.')
+                return redirect('login')
+
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            User.objects.create_user(username=email,
+                                     email=email,
+                                     password=password)
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            messages.error(request,
+                           u'Logowanie zakończyło się niepowodzeniem.')
+            return redirect('login')
+
+        login(request, user)
+        if person.is_member:
+            member_group, c = Group.objects.get_or_create(name='member')
+            member_group.user_set.add(user)
+        if person.is_small_quorum:
+            small_quorum_group, c = Group.objects.get_or_create(
+                name='small_quorum')
+            small_quorum_group.user_set.add(user)
+        if person.is_creator:
+            creator_group, c = Group.objects.get_or_create(name='creator')
+            creator_group.user_set.add(user)
+
+        return redirect('home')
+
+
+
+
--- apps/council/models.py
+++ apps/council/models.py
@@ -1,5 +1,8 @@
 # -*- encoding: utf-8 -*-
+import os
 from django.db import models
+from django.conf import settings
+
 
 TITLES_CHOICES = [
     ('1', u'inż.'),
@@ -13,62 +16,40 @@ TITLES_CHOICES = [
 ]
 
 
-class Meeting(models.Model):
-    date = models.DateTimeField(
-        verbose_name=u'Data spotkania')
-    code = models.CharField(blank=True, null=True, max_length=255)
-
-    def save(self, *args, **kwargs):
-        self.code = u'{}/{}'.format(self.date.isocalendar()[0],
-                                    self.date.isocalendar()[1])
-        super(Meeting, self).save(*args, **kwargs)
-
-    def __unicode__(self):
-        return u'Rada Wydziału Elektroniki {}'.format(self.code)
+def attachment_directory_path(instance, filename):
+    return os.path.join(
+        settings.MEDIA_ROOT, 'uploads',
+        instance._meta.app_label, str(instance.incsalary.pk), filename)
 
 
-class Point(models.Model):
-    meeting = models.ForeignKey(
-        'Meeting',
-        verbose_name=u'Spotkanie')
-    owner = models.ForeignKey(
-        'Member',
-        verbose_name=u'Opiekun')
-    point_type = models.CharField(
-        max_length=255,
-        verbose_name=u'Rodzaj sprawy')
-    title = models.CharField(
-        max_length=255,
-        verbose_name=u'Tytuł')
-    description = models.TextField(
-        verbose_name=u'Opis')
-    is_secret = models.BooleanField(
-        default=False,
-        verbose_name=u'Tajne głosowanie')
-    small_quorum = models.BooleanField(
-        default=False,
-        verbose_name=u'Małe kworum')
-
-
-class Member(models.Model):
+class Person(models.Model):
     first_name = models.CharField(
-        max_length=255,
+        max_length=50,
         verbose_name=u'Imię')
     last_name = models.CharField(
-        max_length=255,
+        max_length=50,
         verbose_name=u'Nazwisko')
     scientific_title = models.CharField(
         max_length=1, blank=True, null=True,
         choices=TITLES_CHOICES,
         verbose_name=u'Tytuł/stopień naukowy')
-    in_small_quorum = models.BooleanField(
-        default=False,
-        verbose_name=u'Członek małego kworum')
     lookup = models.CharField(
-        blank=True, null=True,
-        max_length=255)
+        blank=True,
+        max_length=150)
     email = models.EmailField(
         verbose_name=u'Adres e-mail')
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=u'Aktywne konto')
+    is_creator = models.BooleanField(
+        default=False,
+        verbose_name=u'Konto zarządzające')
+    is_member = models.BooleanField(
+        default=False,
+        verbose_name=u'Członek rady')
+    is_small_quorum = models.BooleanField(
+        default=False,
+        verbose_name=u'Członek małego kworum')
 
     def save(self, *args, **kwargs):
         if self.scientific_title:
@@ -80,7 +61,182 @@ class Member(models.Model):
                 self.in_small_quorum = True
         else:
             self.lookup = u'{} {}'.format(self.first_name, self.last_name)
-        super(Member, self).save(*args, **kwargs)
+        super(Person, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return u'{}'.format(self.lookup)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Nazwa grupy')
+
+    def __unicode__(self):
+        return self.name
+
+
+class FacultyCouncil(models.Model):
+    term = models.PositiveIntegerField(
+        unique=True,
+        verbose_name=u'Kadencja')
+    begin_year = models.PositiveIntegerField(
+        verbose_name=u'Rok rozpoczęcia')
+    end_year = models.PositiveIntegerField(
+        verbose_name=u'Rok zakończenia')
+    dean = models.CharField(
+        max_length=100, verbose_name=u'Dziekan')
+
+    def __unicode__(self):
+        return u'Rada Wydziału {} {}-{}'.format(self.term,
+                                                self.begin_year,
+                                                self.end_year)
+
+
+class FacultyCouncilMember(models.Model):
+    person = models.ForeignKey('Person', verbose_name=u'Osoba')
+    council = models.ForeignKey('FacultyCouncil',
+                                verbose_name=u'Rada wydziału')
+
+    def __unicode__(self):
+        return u'{} {}'.format(self.person, self.council)
+
+
+class Meeting(models.Model):
+    council = models.ForeignKey(
+        'FacultyCouncil',
+        verbose_name=u'Rada Wydziału')
+    date = models.DateField(
+        verbose_name=u'Data spotkania')
+    code = models.CharField(
+        blank=True,
+        max_length=8,
+        verbose_name=u'Kod spotkania')
+    place = models.CharField(
+        blank=True,
+        max_length=255,
+        verbose_name=u'Miejsce spotkania')
+
+    def save(self, *args, **kwargs):
+        self.code = u'{}/{}'.format(self.date.isocalendar()[0],
+                                    self.date.isocalendar()[1])
+        super(Meeting, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return u'Spotkanie Rady Wydziału {}'.format(self.code)
+
+
+class Invited(models.Model):
+    person = models.ForeignKey('Person', verbose_name=u'Osoba')
+    meeting = models.ForeignKey('Meeting',
+                                verbose_name=u'Spotkanie Rady Wydziału')
+    group = models.ForeignKey('Group', verbose_name=u'Grupa')
+    is_present = models.BooleanField(default=False, verbose_name=u'Obecny')
+
+    def __unicode__(self):
+        return u'Zaproszona osoba: {}'.format(self.person)
+
+
+class Access(models.Model):
+    point = models.ForeignKey('Point', verbose_name=u'Punkt spotkania')
+    invited = models.ForeignKey('Invited', verbose_name=u'Zaproszona osoba')
+    group = models.ForeignKey('Group', verbose_name=u'Grupa')
+
+    def __unicode__(self):
+        return u'{} {}'.format(self.point, self.group)
+
+
+class Point(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name=u'Tytuł')
+    meeting = models.ForeignKey(
+        'Meeting',
+        verbose_name=u'Spotkanie')
+    owner = models.ForeignKey(
+        'Invited', null=True,
+        verbose_name=u'Opiekun')
+    category = models.CharField(
+        max_length=50,
+        verbose_name=u'Rodzaj sprawy')
+    description = models.TextField(
+        verbose_name=u'Opis')
+    is_final_agenda = models.BooleanField(
+        default=True,
+        verbose_name=u'Zakwalifikowany')
+
+    def __unicode__(self):
+        return u'Punkt: {}'.format(self.title)
+
+
+class VoteOutcome(models.Model):
+    point = models.ForeignKey(
+        'Point',
+        verbose_name=u'Punkt')
+    description = models.TextField(
+        blank=True,
+        verbose_name=u'Opis głosowania')
+    valid_votes = models.PositiveIntegerField(
+        blank=True, null=True,
+        verbose_name=u'Ważne głosy')
+    yes_votes = models.PositiveIntegerField(
+        blank=True, null=True,
+        verbose_name=u'Głosy za')
+    no_votes = models.PositiveIntegerField(
+        blank=True, null=True,
+        verbose_name=u'Głosy przeciw')
+    abstain_votes = models.PositiveIntegerField(
+        blank=True, null=True,
+        verbose_name=u'Głosy wstrzymania')
+    not_entitled = models.PositiveIntegerField(
+        blank=True, null=True,
+        verbose_name=u'Niepodpisane')
+    is_public = models.BooleanField(
+        default=True,
+        verbose_name=u'Głosowanie publiczne')
+
+    def __unicode__(self):
+        return u'Punkt: {}, za: {}, przeciw: {}, wstrzymało się: {}'.format(
+            self.point.title,
+            self.yes_votes,
+            self.no_votes,
+            self.abstain_votes)
+
+
+class Attachment(models.Model):
+    point = models.ForeignKey(
+        'Point',
+        verbose_name=u'Punkt')
+    file = models.FileField(
+        verbose_name=u'Załączony plik',
+        upload_to=attachment_directory_path,
+        null=True, blank=True)
+    description = models.TextField(
+        blank=True,
+        verbose_name=u'Opis załącznika')
+    resolution = models.ForeignKey(
+        'Resolution',
+        blank=True, null=True,
+        verbose_name=u'Uchwała')
+    meeting = models.ForeignKey(
+        'Meeting',
+        blank=True, null=True,
+        verbose_name=u'Spotkanie')
+
+    def __unicode__(self):
+        return u'{} {}'.format(
+            self.incsalary.title, os.path.basename(self.file.name))
+
+
+class ResolutionPoint(models.Model):
+    resolution = models.ForeignKey(
+        'Resolution',
+        verbose_name=u'Uchwała')
+    point = models.ForeignKey(
+        'Point',
+        verbose_name=u'Punkt')
+
+
+class Resolution(models.Model):
+    title = models.CharField(max_length=250, verbose_name=u'Tytuł')
+    description = models.TextField(verbose_name=u'Opis')
+    number = models.IntegerField(unique=True, verbose_name=u'Numer uchwały')
+    date = models.DateField(auto_now=True, verbose_name=u'Data')
