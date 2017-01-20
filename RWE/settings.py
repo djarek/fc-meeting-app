97d469b00b5ca59ed6fef00e2aedddde6549b6c5
Damian Jarek <damian.jarek93@gmail.com>
2017-01-20 11:51:17 +0100

    Revert change to settings
    This change fixes running the app on production.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- RWE/settings.py
+++ RWE/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-# import dj_database_url
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'RWE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(conn_max_age=600)
-# }
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
 
 # Example sqlite3 database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'db',
+#    }
+#}
 
 
 # Internationalization
